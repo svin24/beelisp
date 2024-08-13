bb7356d960863c932a4c397df68dc9a1a3ab3980
WyattBlue <wyattblue@auto-editor.com>
2024-08-13 00:24:38 -0400

    Add color to error message

--- c/bee.c
+++ c/bee.c
@@ -119,7 +119,7 @@ void lval_expr_print(lval* v, char open, char close){
 void lval_print(lval* v){
     switch (v->type){
         case LVAL_NUM:   printf("%li",v->num); break;
-        case LVAL_ERR:   printf("Error: %s", v->err); break;
+        case LVAL_ERR:   printf("\033[31;40mError: %s\033[0m", v->err); break;
         case LVAL_SYM:   printf("%s", v->sym); break;
         case LVAL_SEXPR: lval_expr_print(v,'(',')'); break;
     }
